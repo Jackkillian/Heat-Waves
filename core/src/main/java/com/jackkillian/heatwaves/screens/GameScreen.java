3c4b7c677e8baae35471958d1f5c895bd50ecdc7
Jack Freund <48269998+Jackkillian@users.noreply.github.com>
2022-06-18 23:13:33 -0600

    Basic tilemap rendering

--- core/src/main/java/com/jackkillian/heatwaves/screens/GameScreen.java
+++ core/src/main/java/com/jackkillian/heatwaves/screens/GameScreen.java
@@ -2,18 +2,27 @@ package com.jackkillian.heatwaves.screens;
 
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.jackkillian.heatwaves.GameData;
 import com.jackkillian.heatwaves.HeatWaves;
 import com.jackkillian.heatwaves.Player;
 import com.jackkillian.heatwaves.systems.HudRenderSystem;
 
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
     private final Engine engine;
     private GameData gameData;
+    private TiledMap map;
+    private OrthographicCamera camera;
+    private OrthogonalTiledMapRenderer renderer;
 
     public GameScreen(HeatWaves game, GameData gameData) {
+        Gdx.input.setInputProcessor(this);
         this.gameData = gameData;
 
         engine = new Engine();
@@ -26,15 +35,23 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
-
+        map = new TmxMapLoader().load("gameMap.tmx");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+        renderer = new OrthogonalTiledMapRenderer(map);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         engine.update(delta);
 
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
     }
 
     @Override
@@ -60,5 +77,45 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
     }
 }
