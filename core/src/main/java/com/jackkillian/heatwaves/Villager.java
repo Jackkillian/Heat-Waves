85c917820b8bafc9ac5c8ba9fbb0b3eb7dad6aaf
Jack Freund <48269998+Jackkillian@users.noreply.github.com>
2022-06-24 22:35:57 -0600

    Better AI

--- core/src/main/java/com/jackkillian/heatwaves/Villager.java
+++ core/src/main/java/com/jackkillian/heatwaves/Villager.java
@@ -3,16 +3,41 @@ package com.jackkillian.heatwaves;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
 
 public class Villager extends NPC {
+    private boolean movingRight = true;
+    private boolean movingLeft;
+
     public Villager(float x, float y) {
         super(new Sprite(new Texture("player/villager.png")), new Texture("player/villagerDead.png"), x, y);
     }
 
     @Override
     public void update(float delta) {
-        if (health > 0) body.setLinearVelocity(new Vector2(MathUtils.random(-100, 100), body.getLinearVelocity().y));
+        if (health > 0) {
+            int random = MathUtils.random(0, 100);
+            int speed = MathUtils.random(1, 50);
+
+            if (movingRight) {
+                if (random > 95) {
+                    movingRight = false;
+                }
+            }
+            if (movingLeft) {
+                if (random > 95) {
+                    movingRight = true;
+                }
+            }
+            movingLeft = !movingRight;
+
+            if (movingRight) {
+                body.setLinearVelocity(speed, body.getLinearVelocity().y);
+                sprite.setFlip(false, false);
+            } else {
+                body.setLinearVelocity(-speed, body.getLinearVelocity().y);
+                sprite.setFlip(true, false);
+            }
+        }
         drawSprite();
         super.update(delta);
     }
