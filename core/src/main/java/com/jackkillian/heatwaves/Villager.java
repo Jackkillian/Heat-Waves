d20c4c5f983bc35634316b9c1bdeacecf8b70fc3
Jack Freund <48269998+Jackkillian@users.noreply.github.com>
2022-06-24 22:15:13 -0600

    yay villagers no longer float

--- core/src/main/java/com/jackkillian/heatwaves/Villager.java
+++ core/src/main/java/com/jackkillian/heatwaves/Villager.java
@@ -7,12 +7,12 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Villager extends NPC {
     public Villager(float x, float y) {
-        super(new Sprite(new Texture("player/villagerIdle1.png")), new Texture("player/henchmanDead.png"), x, y);
+        super(new Sprite(new Texture("player/villager.png")), new Texture("player/villagerDead.png"), x, y);
     }
 
     @Override
     public void update(float delta) {
-        body.setLinearVelocity(new Vector2(MathUtils.random(-100, 100), 0));
+        if (alive) body.setLinearVelocity(new Vector2(MathUtils.random(-100, 100), body.getLinearVelocity().y));
         drawSprite();
         super.update(delta);
     }
